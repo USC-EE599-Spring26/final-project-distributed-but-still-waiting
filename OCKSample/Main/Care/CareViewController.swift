1564137b35fd75ee2903dc4991dabee1ca0b1a64
Phong <70073917+phong-nnguyen@users.noreply.github.com>
2026-03-05 02:11:50 -0800

    Revert "Feat: Added ability to add new Tasks (#2)"
    This reverts commit a8934cb6ab7b8d31c21642647c4cd1ef3ab830a6.

--- OCKSample/Main/Care/CareViewController.swift
+++ OCKSample/Main/Care/CareViewController.swift
@@ -212,11 +212,10 @@ final class CareViewController: OCKDailyPageViewController, @unchecked Sendable 
         query.excludesTasksWithNoEvents = true
         do {
             let tasks = try await store.fetchAnyTasks(query: query)
-            let sortedTasks = tasks.sorted {
-                ($0 as? CareTask)?.priority ?? Int.max <
-                ($1 as? CareTask)?.priority ?? Int.max
+            let orderedTasks = TaskID.ordered.compactMap { orderedTaskID in
+                tasks.first(where: { $0.id == orderedTaskID })
             }
-            return sortedTasks
+            return orderedTasks
         } catch {
             Logger.feed.error("Could not fetch tasks: \(error, privacy: .public)")
             return []
@@ -231,72 +230,6 @@ final class CareViewController: OCKDailyPageViewController, @unchecked Sendable 
         var query = OCKEventQuery(for: date)
         query.taskIDs = [task.id]
 
-        if let task = task as? OCKTask {
-            switch task.card {
-            case .button:
-                let card = OCKButtonLogTaskViewController(
-                    query: query,
-                    store: self.store
-                )
-
-                return [card]
-            case .checklist:
-                let card = OCKChecklistTaskViewController(
-                    query: query,
-                    store: self.store
-                )
-
-                return [card]
-
-            case .grid:
-                let card = OCKGridTaskViewController(
-                    query: query,
-                    store: self.store
-                )
-
-                return [card]
-            case .instruction:
-                let card = EventQueryView<InstructionsTaskView>(
-                    query: query
-                )
-                .formattedHostingController()
-
-                return [card]
-            case .labeledValue:
-                let card = EventQueryView<LabeledValueTaskView>(
-                    query: query
-                )
-                .formattedHostingController()
-
-                return [card]
-
-            case .simple:
-                let card = EventQueryView<SimpleTaskView>(
-                    query: query
-                )
-                .formattedHostingController()
-
-                return [card]
-                
-            default:
-                return nil
-            }
-        } else if let healthKitTask = task as? OCKHealthKitTask {
-            switch healthKitTask.card {
-            case .numericProgress:
-                let card = EventQueryView<NumericProgressTaskView>(
-                    query: query
-                )
-                .formattedHostingController()
-
-                return [card]
-            default:
-                return nil
-            }
-        } else {
-            return nil
-        }
-        /*
         switch task.id {
         case TaskID.steps:
             let card = EventQueryView<NumericProgressTaskView>(
@@ -367,13 +300,8 @@ final class CareViewController: OCKDailyPageViewController, @unchecked Sendable 
             #endif
 
         default:
-            let card = EventQueryView<SimpleTaskView>(
-                query: query
-            )
-            .formattedHostingController()
-
-            return [card]
-        } */
+            return nil
+        }
     }
 
     private func appendTasks(
--- OCKSample/Main/MainTabView.swift
+++ OCKSample/Main/MainTabView.swift
@@ -24,48 +24,47 @@ struct MainTabView: View {
                         Image(systemName: "chart.line.text.clipboard")
                             .renderingMode(.template)
                     } else {
-                        Image(systemName: "chart.line.text.clipboard.fill")
+						Image(systemName: "chart.line.text.clipboard.fill")
                             .renderingMode(.template)
                     }
                 }
                 .tag(0)
 
-            InsightsView()
-                .tabItem {
-                    if selectedTab == 1 {
-                        Image(systemName: "chart.pie.fill")
-                            .renderingMode(.template)
-                    } else {
-                        Image(systemName: "chart.pie")
-                            .renderingMode(.template)
-                    }
-                }
-                .tag(1)
+			InsightsView()
+				.tabItem {
+					if selectedTab == 1 {
+						Image(systemName: "chart.pie.fill")
+							.renderingMode(.template)
+					} else {
+						Image(systemName: "chart.pie")
+							.renderingMode(.template)
+					}
+				}
+				.tag(1)
 
-            ContactView()
-                .tabItem {
-                    if selectedTab == 2 {
-                        Image(systemName: "phone.bubble.fill")
-                            .renderingMode(.template)
-                    } else {
-                        Image(systemName: "phone.bubble")
-                            .renderingMode(.template)
-                    }
-                }
-                .tag(2)
-            
+			ContactView()
+				.tabItem {
+					if selectedTab == 2 {
+						Image(systemName: "phone.bubble.fill")
+							.renderingMode(.template)
+					} else {
+						Image(systemName: "phone.bubble")
+							.renderingMode(.template)
+					}
+				}
+				.tag(2)
 
-            ProfileView(loginViewModel: loginViewModel)
-                .tabItem {
-                    if selectedTab == 3 {
-                        Image(systemName: "person.circle.fill")
-                            .renderingMode(.template)
-                    } else {
-                        Image(systemName: "person.circle")
-                            .renderingMode(.template)
-                    }
-                }
-                .tag(3)
+			ProfileView(loginViewModel: loginViewModel)
+				.tabItem {
+					if selectedTab == 3 {
+						Image(systemName: "person.circle.fill")
+							.renderingMode(.template)
+					} else {
+						Image(systemName: "person.circle")
+							.renderingMode(.template)
+					}
+				}
+				.tag(3)
         }
     }
 }
@@ -73,8 +72,8 @@ struct MainTabView: View {
 struct MainTabView_Previews: PreviewProvider {
     static var previews: some View {
         MainTabView(loginViewModel: .init())
-            .environment(\.appDelegate, AppDelegate())
+			.environment(\.appDelegate, AppDelegate())
             .environment(\.careStore, Utility.createPreviewStore())
-            .careKitStyle(Styler())
+			.careKitStyle(Styler())
     }
 }
