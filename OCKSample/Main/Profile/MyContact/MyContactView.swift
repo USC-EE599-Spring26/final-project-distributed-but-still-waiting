b5cde6a5748c204f976f1f083f0faf9e90859ee7
alexmercel <jai.shah69@gmail.com>
2026-04-03 01:29:36 -0700

    feat: Improve Contact and Profile View

---
+++ OCKSample/Main/Profile/MyContact/MyContactView.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import UIKit
+import CareKit
+import CareKitStore
+import os.log
+
+struct MyContactView: UIViewControllerRepresentable {
+	@Environment(\.careStore) var careStore
+
+	func makeUIViewController(context: Context) -> some UIViewController {
+		let viewController = createViewController()
+		let navigationController = UINavigationController(
+			rootViewController: viewController
+		)
+		return navigationController
+
+	}
+
+	func updateUIViewController(
+		_ uiViewController: UIViewControllerType,
+		context: Context
+	) {}
+
+	func createViewController() -> UIViewController {
+		let viewController = MyContactViewController(store: careStore)
+		return viewController
+	}
+}
+
+struct MyContactView_Previews: PreviewProvider {
+
+	static var previews: some View {
+		MyContactView()
+			.environment(\.careStore, Utility.createPreviewStore())
+			.accentColor(Color.accentColor)
+	}
+}
